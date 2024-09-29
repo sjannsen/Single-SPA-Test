b4757a5e571017462ad95e69610e12e74fb4c630
Sascha <92031835+sjannsen@users.noreply.github.com>
2024-09-29 23:00:53 +0200

    feat: Load frontends dynamically from list

--- Container/src/MEA-root-config.ts
+++ Container/src/MEA-root-config.ts
@@ -1,20 +1,40 @@
-import { registerApplication, start } from "single-spa";
-import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
+import { registerApplication, start } from 'single-spa'
+import { constructApplications, constructRoutes, constructLayoutEngine } from 'single-spa-layout'
 
-const routes = constructRoutes(microfrontendLayout);
+const fetchedMicrofrontends = [
+	{
+		name: '@MEA/React-MicroFrontend',
+		url: '//localhost:8080/MEA-React-MicroFrontend.js',
+	},
+	{
+		name: '@MEA/React-MicroFrontend2',
+		url: '//localhost:8081/MEA-React-MicroFrontend2.js',
+	},
+]
+
+const layoutHTML = `
+		  <single-spa-router>
+			<main>
+			  <route default>
+				<h1>Hello from Container</h1>
+				${fetchedMicrofrontends.map((mf) => `<application name="${mf.name}"></application>`).join('\n')}
+			  </route>
+			</main>
+		  </single-spa-router>
+		`
+
+const parser = new DOMParser()
+const layoutDocument = parser.parseFromString(layoutHTML, 'text/html')
+const routes = constructRoutes(layoutDocument.querySelector('single-spa-router'))
+
 const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
-});
-const layoutEngine = constructLayoutEngine({ routes, applications });
+	routes,
+	loadApp({ name }) {
+		return System.import(name)
+	},
+})
+const layoutEngine = constructLayoutEngine({ routes, applications })
 
-applications.forEach(registerApplication);
-layoutEngine.activate();
-start();
+applications.forEach(registerApplication)
+layoutEngine.activate()
+start()
